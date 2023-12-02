e2eaf56ec1f258721741d1150178755dbf826724
Laissa Cunha <laissa.cunha@ed3.com.br>
2023-12-02 15:55:34 -0300

    feat: adiciona função de alterar a cor do texto

--- js/media.js
+++ js/media.js
@@ -11,6 +11,10 @@
     6. Se a nota for entre 10, mostre na tela: 
       "Hoje é seu aniversário? Pq você ta de parabéns! 10 de média"
 */
+function alteraCorDoTexto(cor) {
+  return document.getElementById("situacaoAluno").style.color = cor;
+}
+
 function calcularMedia() {
   const nota1 = parseFloat(document.getElementById("nota1").value);
   const nota2 = parseFloat(document.getElementById("nota2").value);
@@ -22,20 +26,25 @@ function calcularMedia() {
   // evite mexer no código acima!
 
   if (media === 0) {
+    alteraCorDoTexto('red');
     mensagem = `Infelizmente você zerou a prova :( `;
   } else if (media > 0 && media <= 3) {
+    alteraCorDoTexto('red');
     mensagem = `Caramba, deu ruim, você obteve media ${media}! Estude mais e tente novamente.`;
-    // situacaoAluno.style.color = 'red';
   } else if (media > 3.1 && media < 6) {
+    alteraCorDoTexto('red');
     mensagem = `Você obteve media ${media}! Falta pouco para a média.`;
-    // situacaoAluno.style.color = 'green';
   } else if (media >= 6 && media <= 7) {
+    alteraCorDoTexto('blue');
     mensagem = `Você está na média com ${media}.`;
   } else if (media > 7 && media < 10) {
+    alteraCorDoTexto('green');
     mensagem = `Notão! Sua média é ${media}!`;
   } else if (media === 10) {
+    alteraCorDoTexto('green');
     mensagem = `Hoje é seu aniversário? Pq você ta de parabéns! ${media} de média.`;
   } else if (media > 10) {
+    alteraCorDoTexto('black');
     mensagem = `Ops! Sua média ${media} está acima da média máxima: 10. Verifique se você digitou as notas corretamente.`;
   }
 
