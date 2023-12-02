836c99f35945091f886e173512f88301721fcaed
Laissa Cunha <laissa.cunha@ed3.com.br>
2023-12-02 11:17:45 -0300

    refactor: refatora a condição

--- js/media.js
+++ js/media.js
@@ -12,36 +12,33 @@
       "Hoje é seu aniversário? Pq você ta de parabéns! 10 de média"
 */
 function calcularMedia() {
-    const nota1 = parseFloat(document.getElementById("nota1").value);
-    const nota2 = parseFloat(document.getElementById("nota2").value);
-    const nota3 = parseFloat(document.getElementById("nota3").value);
-    const nota4 = parseFloat(document.getElementById("nota4").value);
+  const nota1 = parseFloat(document.getElementById("nota1").value);
+  const nota2 = parseFloat(document.getElementById("nota2").value);
+  const nota3 = parseFloat(document.getElementById("nota3").value);
+  const nota4 = parseFloat(document.getElementById("nota4").value);
 
-    const media = (nota1 + nota2 + nota3 + nota4)/4
-  
-    let mensagem = "";
-    // evite mexer no código acima!
+  const media = (nota1 + nota2 + nota3 + nota4) / 4;
+  let mensagem = "";
+  // evite mexer no código acima!
 
-    if(media === 0 ) {
-      mensagem = `Infelizmente você zerou a prova :( `
-    } else if (media <= 3) {
-      mensagem = `Caramba, deu ruim, você obteve media ${media}! Estude mais e tente novamente.`
-    } else if (media < 6) {
-      mensagem = `Você obteve media ${media}! Falta pouco para a média.`
-    } else if (media <= 7) {
-      mensagem = `Você está na média com ${media}.`
-    } else if (media < 10) {
-      mensagem = `Notão! Sua média é ${media}!`
-    } else if (media === 10){
-      mensagem = `Hoje é seu aniversário? Pq você ta de parabéns! ${media} de média.`
-    } else {
-      mensagem = `Ops! Sua média ${media} está acima da média máxima: 10. Verifique se você digitou as notas corretamente.`
-    }
-  
-  
-  
-  
-  
-    // evite mexer na linha de código abaixo!
-    document.getElementById("situacaoAluno").innerHTML = mensagem;
+  if (media === 0) {
+    mensagem = `Infelizmente você zerou a prova :( `;
+  } else if (media > 0 && media <= 3) {
+    mensagem = `Caramba, deu ruim, você obteve media ${media}! Estude mais e tente novamente.`;
+    // situacaoAluno.style.color = 'red';
+  } else if (media > 3.1 && media < 6) {
+    mensagem = `Você obteve media ${media}! Falta pouco para a média.`;
+    // situacaoAluno.style.color = 'green';
+  } else if (media >= 6 && media <= 7) {
+    mensagem = `Você está na média com ${media}.`;
+  } else if (media > 7 && media < 10) {
+    mensagem = `Notão! Sua média é ${media}!`;
+  } else if (media === 10) {
+    mensagem = `Hoje é seu aniversário? Pq você ta de parabéns! ${media} de média.`;
+  } else if (media > 10) {
+    mensagem = `Ops! Sua média ${media} está acima da média máxima: 10. Verifique se você digitou as notas corretamente.`;
   }
+
+  // evite mexer na linha de código abaixo!
+  document.getElementById("situacaoAluno").innerHTML = mensagem;
+}
